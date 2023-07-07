7fb4c3de18ce5ab124535835a90e79e708784f1a
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-07-08 02:57:14 +0300

    Create DatabaseInitService.java

---
+++ src/main/java/org/example/DatabaseInitService.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseInitService {
+
+    public static void main(String[] args) {
+        try {
+            Database database = Database.getInstance();
+            Connection connection = database.getConnection();
+            executeSqlFile(connection, "sql/init_db.sql");
+            database.closeConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void executeSqlFile(Connection connection, String filePath) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            StringBuilder sql = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                sql.append(line);
+                if (line.endsWith(";")) {
+                    String query = sql.toString();
+                    try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                        preparedStatement.execute();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    sql.setLength(0);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
