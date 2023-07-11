5cbe680da916459ab2f4df4961fc8821cd34c6eb
Yareishe <121358447+Yareishe@users.noreply.github.com>
2023-07-12 01:55:50 +0300

    Update DatabaseInitService.java

--- src/main/java/org/example/DatabaseInitService.java
+++ src/main/java/org/example/DatabaseInitService.java
@@ -3,10 +3,7 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DatabaseInitService {
 
@@ -22,7 +19,8 @@ public class DatabaseInitService {
     }
 
     private static void executeSqlFile(Connection connection, String filePath) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath));
+             Statement statement = connection.createStatement()) {
             StringBuilder sql = new StringBuilder();
             String line;
 
@@ -30,16 +28,28 @@ public class DatabaseInitService {
                 sql.append(line);
                 if (line.endsWith(";")) {
                     String query = sql.toString();
-                    try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-                        preparedStatement.setString(1, "John26.0");
-                        preparedStatement.execute();
-                    } catch (SQLException e) {
-                        e.printStackTrace();
+                    if (query.contains("SELECT * FROM worker")) {
+                        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                            preparedStatement.setString(1,"John26.0");
+                            ResultSet rs = preparedStatement.executeQuery();
+                                String name = rs.getString("name");
+                                System.out.println(name);
+                            preparedStatement.execute();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    else {
+                        statement.execute(sql.toString());
+                        ResultSet rs = statement.executeQuery("SELECT * FROM worker");
+                            String name = rs.getString("name");
+                            System.out.println(name);
+
                     }
                     sql.setLength(0);
                 }
             }
-        } catch (IOException e) {
+        } catch (IOException | SQLException e) {
             e.printStackTrace();
         }
     }
